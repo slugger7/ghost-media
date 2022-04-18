e481714a8fe47073ea5569b5d844db33a94d5dc2
Kevin Heritage <kheritage222@gmail.com>
2022-04-18 18:05:54 +0200

    Add tooltip to title

--- Ghost.Web/ClientApp/src/components/VideoCard.jsx
+++ Ghost.Web/ClientApp/src/components/VideoCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
-import { Card, CardActionArea, CardHeader, CardMedia, Typography, IconButton } from '@mui/material'
+import { Card, CardActionArea, CardHeader, CardMedia, Typography, IconButton, Tooltip } from '@mui/material'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 export const VideoCard = ({ id, title }) => <Card sx={{ maxHeight: '400px' }}>
@@ -14,7 +14,7 @@ export const VideoCard = ({ id, title }) => <Card sx={{ maxHeight: '400px' }}>
     />
     <CardHeader
       className="ghost-video-card-header"
-      title={<Typography variant="h6" component="h6" noWrap={true}>{title}</Typography>}
+      title={<Tooltip title={title}><Typography variant="h6" component="h6" noWrap={true}>{title}</Typography></Tooltip>}
       disableTypography={true}
       action={<IconButton>
         <MoreVertIcon />
