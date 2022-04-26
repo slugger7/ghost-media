61ea909b245fd165f56eac42788a08b060bc1b44
Kevin Heritage <kheritage222@gmail.com>
2022-04-26 07:50:22 +0200

    change name of base url for axios to server url

--- Ghost.Web/ClientApp/src/index.js
+++ Ghost.Web/ClientApp/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
-import reportWebVitals from './reportWebVitals'
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+// import reportWebVitals from './reportWebVitals'
 import axios from 'axios'
 import { head } from 'ramda'
 
@@ -10,23 +10,23 @@ const container = document.getElementById('root')
 const root = createRoot(container)
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href')
-const hostUrl = [
+const serverUrl = [
   window.location.protocol,
   '//',
   head(window.location.host.split(':')),
   ':7110',
   '/api',
 ].join('')
-axios.defaults.baseURL = hostUrl
+axios.defaults.baseURL = serverUrl
 
 root.render(<App baseUrl={baseUrl} />)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister()
+//serviceWorkerRegistration.unregister()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+//reportWebVitals()
