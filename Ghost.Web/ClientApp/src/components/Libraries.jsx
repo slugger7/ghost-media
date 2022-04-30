9434e982a17e0ce26a5381520fd41b93561ebf71
Kevin Heritage <kheritage222@gmail.com>
2022-04-30 15:06:27 +0200

    Add skeleton to libraries loading

--- Ghost.Web/ClientApp/src/components/Libraries.jsx
+++ Ghost.Web/ClientApp/src/components/Libraries.jsx
@@ -4,19 +4,19 @@ import axios from 'axios'
 
 import { ButtonLink } from './ButtonLink.jsx';
 import { LibraryCard } from './LibraryCard.jsx'
-import { Container, Stack } from '@mui/material';
+import { Container, Stack, Skeleton, Paper } from '@mui/material';
 
 const fetchLibraries = async () => (await axios.get("library")).data
 
 export const Libraries = () => {
   const librariesPage = useAsync(fetchLibraries, [])
 
-  // TODO: Make skeleton
   return (<Container>
-    <Stack direction="column" spacing={1} sx={{ mb: 1 }}>
-      {librariesPage.loading && <span>loading ...</span>}
-      {!librariesPage.loading && librariesPage.result?.content?.map(library => (<LibraryCard key={library._id} library={library} refresh={librariesPage.execute} />))}
-    </Stack>
+    {librariesPage.loading && <Skeleton height="90px" />}
+    {!librariesPage.loading && <Stack direction="column" spacing={1} sx={{ mb: 1 }}>
+      {librariesPage.result?.content?.map(library => (<LibraryCard key={library._id} library={library} refresh={librariesPage.execute} />))}
+      {librariesPage.result?.content.length === 0 && <Paper elevation={1} sx={{ my: 1, p: 2 }}>Nothing here, try adding a library.</Paper>}
+    </Stack>}
     <ButtonLink to="/libraries/add" variant="contained">Add Library</ButtonLink>
-  </Container>)
+  </Container >)
 } 
