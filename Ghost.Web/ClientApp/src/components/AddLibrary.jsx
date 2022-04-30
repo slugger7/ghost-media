7bc36edc9fa26061e40fc1bc46d859632637862e
Kevin Heritage <kheritage222@gmail.com>
2022-04-30 15:11:14 +0200

    Remove defaults in adding library

--- Ghost.Web/ClientApp/src/components/AddLibrary.jsx
+++ Ghost.Web/ClientApp/src/components/AddLibrary.jsx
@@ -25,8 +25,8 @@ const createPaths = async ({ _id, paths }) => (await axios.put(`library/${_id}/a
 
 export const AddLibrary = () => {
   const navigate = useNavigate()
-  const [libraryName, setLibraryName] = useState('test lib name');
-  const [chosenPaths, setChosenPaths] = useState(["/home/slugger/dev/ghost-media/assets"]);
+  const [libraryName, setLibraryName] = useState();
+  const [chosenPaths, setChosenPaths] = useState([]);
   const [addingPath, setAddingPath] = useState(false);
   const [submitting, setSubmitting] = useState(false);
 
