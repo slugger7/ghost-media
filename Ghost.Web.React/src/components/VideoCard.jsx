2711c678c51d9565307f400291e77aacc87a9dfa
Kevin Heritage <kheritage222@protonmail.com>
2022-06-27 18:59:47 +0200

    Create actors and favourites on video cards

--- Ghost.Web.React/src/components/VideoCard.jsx
+++ Ghost.Web.React/src/components/VideoCard.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
-import { Card, CardActionArea, CardHeader, CardMedia, Typography, IconButton, Tooltip, Skeleton, CardActions } from '@mui/material'
+import { Card, CardActionArea, CardHeader, CardMedia, Stack, Typography, IconButton, Tooltip, Skeleton, CardActions, Chip } from '@mui/material'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 import { items, VideoMenu } from './VideoMenu.jsx'
 import { generateVideoUrl, toggleFavourite } from '../services/video.service.js'
@@ -34,18 +34,34 @@ export const VideoCard = ({ video, remove }) => {
     }
   }
 
+  const actors = localVideo.actors.length > 0 ? localVideo.actors.map(a => <Chip
+    size='small'
+    key={a.id}
+    label={a.name}
+    variant="outlined"
+    color="primary"
+    component={Link}
+    to={`/actors/${a.id}/${encodeURIComponent(a.name.toLowerCase())}`}
+    clickable />) : <Chip variant="outlined" label="Unknown" size='small'></Chip>
+
   const urlToMedia = `/media/${localVideo.id}/${localVideo.title}`;
-  return (<Card sx={{ maxHeight: '400px' }}>
+  return (<Card sx={{
+    maxHeight: '400px',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'column'
+  }}>
     <CardHeader
       className="ghost-video-card-header"
       title={<Tooltip title={localVideo.title}><Typography variant="h6" noWrap={true}>
         <Typography component={Link} to={urlToMedia} color="white">{localVideo.title}</Typography>
       </Typography></Tooltip>}
-      subheader={JSON.stringify(localVideo.actors)}
+      subheader={<Stack direction="row" spacing={0.5}>{actors}</Stack >}
       disableTypography={true}
     />
     <CardActionArea LinkComponent={Link} to={urlToMedia}>
-      {localVideo.thumbnail && <CardMedia
+      {localVideo.thumbnail && <CardMedia sx={{ maxHeight: '200px' }}
         component="img"
         image={`${axios.defaults.baseURL}/image/${localVideo.thumbnail.id}/${localVideo.thumbnail.name}`}
         alt={localVideo.title}
