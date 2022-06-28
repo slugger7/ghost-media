0ddd7c3b246e12644132436c31f38129731c611c
Kevin Heritage <kheritage222@protonmail.com>
2022-06-28 17:31:36 +0200

    Search through actors

--- Ghost.Web.React/src/components/Actors.jsx
+++ Ghost.Web.React/src/components/Actors.jsx
@@ -1,18 +1,33 @@
-import { Chip, } from '@mui/material'
-import React from 'react'
+import { Box, Chip, } from '@mui/material'
+import { includes } from 'ramda'
+import React, { useState, useEffect } from 'react'
 import { useAsync } from 'react-async-hook'
 import { Link } from 'react-router-dom'
 import { fetchActors } from '../services/actor.service'
 import { ChipSkeleton } from './ChipSkeleton'
 import { NothingHere } from './NothingHere'
+import { Search } from './Search'
 
 export const Actors = () => {
   const actorsResult = useAsync(fetchActors, [])
+  const [filteredActors, setFilteredActors] = useState([]);
+  const [search, setSearch] = useState('');
 
-  return <>
+  useEffect(() => {
+    if (actorsResult.result) {
+      setFilteredActors(actorsResult.result.filter(
+        actor => includes(search.trim().toLowerCase(), actor.name.toLowerCase())
+      ));
+    }
+  }, [search, actorsResult.result])
+
+  return <Box sx={{ my: 1 }}>
+    <Box sx={{ mb: 1 }}>
+      <Search search={search} setSearch={setSearch} />
+    </Box>
     {actorsResult.loading && <ChipSkeleton />}
     {!actorsResult.loading && <>
-      {actorsResult.result.map(actor => <Chip
+      {filteredActors.map(actor => <Chip
         sx={{ m: 0.5 }}
         key={actor.id}
         label={`${actor.name} ${actor.videoCount}`}
@@ -22,7 +37,7 @@ export const Actors = () => {
         to={`/actors/${actor.id}/${encodeURIComponent(actor.name.toLowerCase())}`}
         clickable
       />)}
-      {actorsResult.result.length === 0 && <NothingHere>Nothing here. Add some actors to videos.</NothingHere>}
+      {filteredActors.length === 0 && <NothingHere>Nothing here. Add some actors to videos.</NothingHere>}
     </>}
-  </>
+  </Box>
 }
