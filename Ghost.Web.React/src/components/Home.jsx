2f700abe4ebbe41696dc49bbd6ac5fab4315aac1
Kevin Heritage <kevin.heritage@rabobank.nl>
2022-10-03 17:27:45 +0200

    feat: remove pagination from query on home

--- Ghost.Web.React/src/components/Home.jsx
+++ Ghost.Web.React/src/components/Home.jsx
@@ -1,25 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
 
-import { VideoGrid } from './VideoGrid.jsx';
+import { VideoGrid } from './VideoGrid.jsx'
 import { Sort } from './Sort.jsx'
 import { fetchVideos } from '../services/video.service'
-import { updateSearchParamsService, getSearchParamsObject } from '../services/searchParam.service.js';
-import usePromise from '../services/use-promise.js';
-
+import usePromise from '../services/use-promise.js'
 
 export const Home = () => {
-  const [page, setPage] = useState()
-  const [limit, setLimit] = useState()
-  const [search, setSearch] = useState('');
-  const [total, setTotal] = useState(0);
-  const [searchParams, setSearchParams] = useSearchParams()
-  const [sortBy, setSortBy] = useState('date-added');
-  const [sortAscending, setSortAscending] = useState(true);
+  const [page, setPage] = useState(1)
+  const [limit] = useState(48)
+  const [search, setSearch] = useState('')
+  const [total, setTotal] = useState(0)
+  const [sortBy, setSortBy] = useState('date-added')
+  const [sortAscending, setSortAscending] = useState(false)
   const [videosPage, error, loading] = usePromise(
     () => fetchVideos(page, limit, search, sortBy, sortAscending),
-    [page, limit, search, sortBy, sortAscending]
-  );
+    [page, limit, search, sortBy, sortAscending],
+  )
 
   useEffect(() => {
     if (!loading && !error) {
@@ -27,43 +23,27 @@ export const Home = () => {
     }
   }, [videosPage, error, loading])
 
-  useEffect(() => {
-    const params = getSearchParamsObject(searchParams);
-    setLimit(params.limit || limit || 48)
-    setPage(params.page || page || 1)
-    setSearch(params.search || search)
-    setSortBy(params.sortBy || sortBy)
-    setSortAscending(params.ascending === undefined ? sortBy : params.ascending)
-  }, [searchParams])
-
-  const updateSearchParams = updateSearchParamsService(
-    setSearchParams,
-    { page, limit, search, sortBy, ascending: sortAscending }
-  );
-
-  const handleSearchChange = (searchValue) => {
-    updateSearchParams({
-      search: encodeURIComponent(searchValue),
-      page: 0,
-    })
-  }
-
-  const sortComponent = <Sort
-    sortBy={sortBy}
-    setSortBy={(sortByValue) => updateSearchParams({ sortBy: sortByValue })}
-    sortDirection={sortAscending}
-    setSortDirection={(sortAscendingValue) => updateSearchParams({ ascending: sortAscendingValue })} />
-
-  return (<>
-    <VideoGrid
-      videos={videosPage?.content}
-      loading={loading}
-      onPageChange={(e, newPage) => updateSearchParams({ page: newPage })}
-      page={page}
-      count={Math.ceil(total / limit) || 1}
-      search={search}
-      setSearch={handleSearchChange}
-      sortComponent={sortComponent}
+  const sortComponent = (
+    <Sort
+      sortBy={sortBy}
+      setSortBy={setSortBy}
+      sortDirection={sortAscending}
+      setSortDirection={setSortAscending}
     />
-  </>)
+  )
+
+  return (
+    <>
+      <VideoGrid
+        videos={videosPage?.content}
+        loading={loading}
+        onPageChange={(e, newPage) => setPage(newPage)}
+        page={page}
+        count={Math.ceil(total / limit) || 1}
+        search={search}
+        setSearch={setSearch}
+        sortComponent={sortComponent}
+      />
+    </>
+  )
 }
--- Ghost.Web.React/src/components/VideoGrid.jsx
+++ Ghost.Web.React/src/components/VideoGrid.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Grid, Pagination } from '@mui/material'
 import { remove } from 'ramda'
 
 import { VideoCard } from './VideoCard.jsx'
 import { VideoCardSkeleton } from './VideoCardSkeleton.jsx'
-import { Search } from './Search';
+import { Search } from './Search'
 import { NothingHere } from './NothingHere.jsx'
 
-const removeVideo = ({ index, setVideos }) => () =>
-  setVideos(remove(index, 1))
+const removeVideo =
+  ({ index, setVideos }) =>
+  () =>
+    setVideos(remove(index, 1))
 
 export const VideoGrid = ({
   videos = [],
@@ -19,51 +21,78 @@ export const VideoGrid = ({
   onPageChange,
   setSearch,
   search,
-  sortComponent
+  sortComponent,
 }) => {
-  const paginationComponent = <>
-    {count > 1 && page && <Grid item xs={12} md={6} lg={5}>
-      <Pagination
-        size="small"
-        color="primary"
-        page={page}
-        defaultPage={1}
-        count={count}
-        showFirstButton showLastButton
-        onChange={onPageChange}
-      /></Grid>
-    }
-  </>
+  const paginationComponent = (
+    <>
+      {count > 1 && page && (
+        <Grid item xs={12} md={6} lg={5}>
+          <Pagination
+            size="small"
+            color="primary"
+            page={page}
+            defaultPage={1}
+            count={count}
+            showFirstButton
+            showLastButton
+            onChange={onPageChange}
+          />
+        </Grid>
+      )}
+    </>
+  )
 
-  return <>
-    <Grid
-      container
-      alignItems="center"
-      direction="row"
-      sx={{ my: 1 }}
-      spacing={1}>
-      <Grid item xs={12} md={6} lg={4}>
-        <Search search={search} setSearch={setSearch} />
-      </Grid>
-      {paginationComponent}
-      {sortComponent && <Grid item xs={12} md={6} lg={3}>
-        {sortComponent}
-      </Grid>}
-    </Grid>
-    <Grid container spacing={2}>
-      {loading && <Grid item xs={12} sm={6} md={4} lg={3} xl={2}><VideoCardSkeleton /></Grid>}
-      {!loading && videos.length === 0 && <Grid item xs={12}><NothingHere>Nothing here. Add a library and sync it to have videos appear here</NothingHere></Grid>}
-
-      {!loading && videos && videos.map((video, index) => <Grid key={video.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
-        <VideoCard video={video} remove={removeVideo({ index, setVideos: () => {} })} />
-      </Grid>)}
-    </Grid>
-    <Grid container alignItems="center" direction="row" sx={{ my: 1 }}>
-      <Grid item md={12}>
+  return (
+    <>
+      <Grid
+        container
+        alignItems="center"
+        direction="row"
+        sx={{ my: 1 }}
+        spacing={1}
+      >
+        <Grid item xs={12} md={6} lg={4}>
+          <Search search={search} setSearch={setSearch} />
+        </Grid>
         {paginationComponent}
+        {sortComponent && (
+          <Grid item xs={12} md={6} lg={3}>
+            {sortComponent}
+          </Grid>
+        )}
       </Grid>
-    </Grid>
-  </>
+      <Grid container spacing={2}>
+        {loading && (
+          <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+            <VideoCardSkeleton />
+          </Grid>
+        )}
+        {!loading && videos.length === 0 && (
+          <Grid item xs={12}>
+            <NothingHere>
+              Nothing here. Add a library and sync it to have videos appear here
+            </NothingHere>
+          </Grid>
+        )}
+
+        {!loading &&
+          videos &&
+          videos.map((video, index) => (
+            <Grid key={video.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
+              <VideoCard
+                video={video}
+                remove={removeVideo({ index, setVideos: () => {} })}
+              />
+            </Grid>
+          ))}
+      </Grid>
+      <Grid container alignItems="center" direction="row" sx={{ my: 1 }}>
+        <Grid item md={12}>
+          {paginationComponent}
+        </Grid>
+      </Grid>
+    </>
+  )
 }
 
 VideoGrid.propTypes = {
@@ -74,5 +103,5 @@ VideoGrid.propTypes = {
   onPageChange: PropTypes.func.isRequired,
   search: PropTypes.string.isRequired,
   setSearch: PropTypes.func.isRequired,
-  sortComponent: PropTypes.node
+  sortComponent: PropTypes.node,
 }
