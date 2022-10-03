4b4aae549656512a34aadeae1f32361624471f7a
Kevin Heritage <kevin.heritage@rabobank.nl>
2022-10-03 18:13:35 +0200

    feat: do not show favourites when user is not chosen

--- Ghost.Web.React/src/components/NavMenu.jsx
+++ Ghost.Web.React/src/components/NavMenu.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import { Box, Tabs } from '@mui/material'
-import HomeIcon from '@mui/icons-material/Home';
-import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
-import PeopleIcon from '@mui/icons-material/People';
-import SettingsIcon from '@mui/icons-material/Settings';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import HomeIcon from '@mui/icons-material/Home'
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy'
+import PeopleIcon from '@mui/icons-material/People'
+import SettingsIcon from '@mui/icons-material/Settings'
+import FavoriteIcon from '@mui/icons-material/Favorite'
 
 import { LinkTab } from './LinkTab.jsx'
 
 export const NavMenu = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0)
+  const userId = localStorage.getItem('userId') // needs to be changed to a provider
 
   const handleTabChange = (_, newValue) => {
     setValue(newValue)
@@ -23,14 +24,42 @@ export const NavMenu = () => {
           onChange={handleTabChange}
           aria-label="nav tabs scrollable auto"
           variant="scrollable"
-          scrollButtons="auto">
-          <LinkTab to="/" label="Home" icon={<HomeIcon />} iconPosition="start" />
-          <LinkTab to="/genres" label="Genres" icon={<TheaterComedyIcon />} iconPosition="start" />
-          <LinkTab to="/actors" label="Actors" icon={<PeopleIcon />} iconPosition="start" />
-          <LinkTab to="/favourites" label="Favourites" icon={<FavoriteIcon />} iconPosition="start" />
-          <LinkTab to="/settings" label="Settings" icon={<SettingsIcon />} iconPosition="start" />
+          scrollButtons="auto"
+        >
+          <LinkTab
+            to="/"
+            label="Home"
+            icon={<HomeIcon />}
+            iconPosition="start"
+          />
+          <LinkTab
+            to="/genres"
+            label="Genres"
+            icon={<TheaterComedyIcon />}
+            iconPosition="start"
+          />
+          <LinkTab
+            to="/actors"
+            label="Actors"
+            icon={<PeopleIcon />}
+            iconPosition="start"
+          />
+          {userId && (
+            <LinkTab
+              to="/favourites"
+              label="Favourites"
+              icon={<FavoriteIcon />}
+              iconPosition="start"
+            />
+          )}
+          <LinkTab
+            to="/settings"
+            label="Settings"
+            icon={<SettingsIcon />}
+            iconPosition="start"
+          />
         </Tabs>
       </Box>
     </header>
-  );
+  )
 }
