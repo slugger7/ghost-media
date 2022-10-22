bf02a9fe6d5567d5407eeba275fa2a48f5f461f1
Kevin Heritage <kheritage222@protonmail.com>
2022-10-22 11:35:01 +0200

    chore: remove some warnings

--- Ghost.Repository/Extensions/FilterGenres.cs
+++ Ghost.Repository/Extensions/FilterGenres.cs
@@ -6,22 +6,29 @@ namespace Ghost.Repository.Extensions
     {
         public static IEnumerable<Video> FilterGenres(this IEnumerable<Video> videos, string[]? genres)
         {
-            if (genres == null || (genres != null && genres.Count() == 0))
+            if (genres == null)
             {
                 return videos;
             }
+            else
+            {
+                if (genres.Count() == 0)
+                {
+                    return videos;
+                }
 
-            return videos
-                .Where(video => genres
-                    .ToArray()
-                    .All(genre => video.VideoGenres
-                        .Any(
-                            videoGenre => videoGenre.Genre.Name
-                                .ToLower()
-                                .Equals(genre.ToLower())
+                return videos
+                    .Where(video => genres
+                        .ToArray()
+                        .All(genre => video.VideoGenres
+                            .Any(
+                                videoGenre => videoGenre.Genre.Name
+                                    .ToLower()
+                                    .Equals(genre.ToLower())
+                            )
                         )
-                    )
-                );
+                    );
+            }
         }
     }
 }
