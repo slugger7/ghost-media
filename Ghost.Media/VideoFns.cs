8a54687549d1ec0d916aaef964dc51d30b4e9e80
Kevin Heritage <kheritage222@protonmail.com>
2023-02-18 09:56:14 +0100

    create sub video video fn

--- Ghost.Media/VideoFns.cs
+++ Ghost.Media/VideoFns.cs
@@ -3,23 +3,28 @@ using Ghost.Dtos;
 
 namespace Ghost.Media
 {
-  public static class VideoFns
-  {
-    public static VideoMetaDataDto? GetVideoInformation(string path)
+    public static class VideoFns
     {
-      var mediaInfo = FFProbe.Analyse(path);
-      if (mediaInfo.PrimaryVideoStream == null) return default;
-      var fileInfo = new FileInfo(path);
-      return new VideoMetaDataDto
-      {
-        Duration = mediaInfo.Duration,
-        Format = mediaInfo.Format.FormatName,
-        FormatLong = mediaInfo.Format.FormatLongName,
-        Width = mediaInfo.PrimaryVideoStream.Width,
-        Height = mediaInfo.PrimaryVideoStream.Height,
-        Created = fileInfo.CreationTimeUtc,
-        Size = fileInfo.Length
-      };
+        public static VideoMetaDataDto? GetVideoInformation(string path)
+        {
+            var mediaInfo = FFProbe.Analyse(path);
+            if (mediaInfo.PrimaryVideoStream == null) return default;
+            var fileInfo = new FileInfo(path);
+            return new VideoMetaDataDto
+            {
+                Duration = mediaInfo.Duration,
+                Format = mediaInfo.Format.FormatName,
+                FormatLong = mediaInfo.Format.FormatLongName,
+                Width = mediaInfo.PrimaryVideoStream.Width,
+                Height = mediaInfo.PrimaryVideoStream.Height,
+                Created = fileInfo.CreationTimeUtc,
+                Size = fileInfo.Length
+            };
+        }
+
+        public static async Task<bool> CreateSubVideoAsync(string inputPath, string outputPath, TimeSpan start, TimeSpan end)
+        {
+            return await FFMpeg.SubVideoAsync(inputPath, outputPath, start, end);
+        }
     }
-  }
 }
