4b007d2d4a711f956f34d0fbf18e9c2e2580c3fe
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2023-12-22 20:22:37 +0100

    create playlist created

--- Ghost.Web/src/pages/CreatePlaylist.jsx
+++ Ghost.Web/src/pages/CreatePlaylist.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import { createPlaylist } from "../services/playlists.service";
 import { useNavigate } from "react-router";
+import { Typography, Grid, TextField, Button } from "@mui/material";
 
 export const CreatePlaylist = () => {
   const [name, setName] = useState('');
@@ -13,20 +14,22 @@ export const CreatePlaylist = () => {
 
     try {
       await createPlaylist({ name })
+
+      navigate(-1)
     } finally {
       setLoading(false)
     }
   }
   return <>
-    <h1>Create a new playlist</h1>
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Name</label>
-      <input 
-        type="text" 
-        id="name" 
-        value={name} 
-        onChange={(e) => setName(e.target.value)} />
-      <button type="submit">Create</button>
-    </form>
+    <Grid container spacing={1}>
+      <Grid item xs={12}><Typography variant="h3">New Playlist</Typography></Grid>
+      <Grid item xs={12}>
+        <TextField label="Name" fullWidth value={name} onChange={e => setName(e.target.value)} />
+      </Grid>
+      <Grid item xs={12} sx={{display: 'flex', justifyContent: 'end', gap: 1}}>
+        <Button variant="outlined" onClick={() => navigate(-1)}>Cancel</Button>
+        <Button variant="contained" onClick={handleSubmit} disabled={loading}>Create</Button>
+      </Grid>
+    </Grid>
   </>
 }
