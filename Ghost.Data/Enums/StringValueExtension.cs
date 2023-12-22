adcbb0be00c6fd053e1665417fa1d5788e16a0e7
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2023-12-22 13:56:16 +0100

    remove warning

--- Ghost.Data/Enums/StringValueExtension.cs
+++ Ghost.Data/Enums/StringValueExtension.cs
@@ -4,19 +4,20 @@ namespace Ghost.Data.Enums;
 
 public static class StringValueExtension
 {
-    public static string GetStringValue(this Enum value)
-    {
-        // Get the type
-        Type type = value.GetType();
+  public static string? GetStringValue(this Enum value)
+  {
+    // Get the type
+    Type type = value.GetType();
 
-        // Get fieldinfo for this type
-        FieldInfo? fieldInfo = type.GetField(value.ToString());
+    // Get fieldinfo for this type
+    FieldInfo? fieldInfo = type.GetField(value.ToString());
 
-        // Get the stringvalue attributes
-        StringValueAttribute[]? attribs = fieldInfo?.GetCustomAttributes(
-            typeof(StringValueAttribute), false) as StringValueAttribute[];
+    // Get the stringvalue attributes
+    StringValueAttribute[]? attribs = fieldInfo?.GetCustomAttributes(
+        typeof(StringValueAttribute), false) as StringValueAttribute[];
 
-        // Return the first if there was a match.
-        return attribs.Length > 0 ? attribs[0].StringValue : null;
-    }
+    // Return the first if there was a match.
+    if (attribs == null) return null;
+    return attribs.Length > 0 ? attribs[0].StringValue : null;
+  }
 }
